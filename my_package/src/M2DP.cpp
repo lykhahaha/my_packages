1a42e139258bde310d732d71ad41dde1a2712284
phamngtuananh <phamngtuananh@gmail.com>
2018-01-01 22:24:07 +0800

    Update M2DP descriptor computation

--- my_package/src/M2DP.cpp
+++ my_package/src/M2DP.cpp
@@ -3,6 +3,7 @@
 
 #include <pcl/common/common.h>
 #include <pcl/common/centroid.h>
+#include <pcl/common/pca.h>
 #include <pcl/io/pcd_io.h>
 
 int main(int argc, char** argv)
@@ -13,8 +14,9 @@ int main(int argc, char** argv)
     return(-1);
   }
 
+  // Get input
   std::string f_pcd = argv[1];
-  pcl::PointCloud<pcl::PointXYZI> input_cloud;
+  pcl::PointCloud<pcl::PointXYZI> input_cloud, processed_cloud;
   if(pcl::io::loadPCDFile<pcl::PointXYZI>(f_pcd, input_cloud) == -1)
   {
     std::cout << "Couldn't read pcd " << f_pcd << "." << std::endl;
@@ -22,12 +24,19 @@ int main(int argc, char** argv)
   }
   std::cout << "Loaded " << input_cloud.size() << " data points from " << f_pcd << std::endl;
 
+  // Demean cloud
   Eigen::Vector4d cloud_centroid; // (x, y, z, 1)
   if(!pcl::compute3DCentroid(input_cloud, cloud_centroid))
   {
     std::cout << "Failed to compute cloud centroid." << std::endl;
     return(-1);
   }
+  pcl::demeanPointCloud(input_cloud, cloud_centroid, processed_cloud);
+
+  // Do PCA and re-alignment
+  pcl::PCA<pcl::PointXYZI> pca(false);
+  pca.setInputCloud(processed_cloud.makeShared());
+  std::cout << pca.getEigenVectors() << std::endl;
 
   return(0);
 }
