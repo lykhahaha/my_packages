67623a0cbd0933b7df573670c2c9333ca34ff5a1
phamngtuananh <phamngtuananh@gmail.com>
2018-01-02 17:52:29 +0800

    Update

--- my_package/src/M2DP.cpp
+++ my_package/src/M2DP.cpp
@@ -1,10 +1,15 @@
+// Reference: https://github.com/LiHeUA/M2DP/blob/master/M2DP.m
 #include <iostream>
 #include <string>
 
 #include <pcl/common/common.h>
 #include <pcl/common/centroid.h>
+#include <pcl/common/transforms.h>
 #include <pcl/common/pca.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 #include <pcl/io/pcd_io.h>
+#include <Eigen/Dense>
 
 int main(int argc, char** argv)
 {
@@ -16,7 +21,7 @@ int main(int argc, char** argv)
 
   // Get input
   std::string f_pcd = argv[1];
-  pcl::PointCloud<pcl::PointXYZI> input_cloud, processed_cloud;
+  pcl::PointCloud<pcl::PointXYZI> input_cloud;
   if(pcl::io::loadPCDFile<pcl::PointXYZI>(f_pcd, input_cloud) == -1)
   {
     std::cout << "Couldn't read pcd " << f_pcd << "." << std::endl;
@@ -24,22 +29,42 @@ int main(int argc, char** argv)
   }
   std::cout << "Loaded " << input_cloud.size() << " data points from " << f_pcd << std::endl;
 
-  // Demean cloud
+  // Do PCA, demean and re-align x/y-axis
+  pcl::PCA<pcl::PointXYZI> pca(false);
+  pca.setInputCloud(input_cloud.makeShared());
+
+  Eigen::MatrixXd processed_cloud;
+  pcl::demeanPointCloud(input_cloud, pca.getMean().cast<double>(), processed_cloud);
+  
+  Eigen::MatrixXd X_values = pca.getEigenVectors().cast<double>().col(0).transpose() * processed_cloud.topRows(3);
+  Eigen::MatrixXd Y_values = pca.getEigenVectors().cast<double>().col(1).transpose() * processed_cloud.topRows(3);
+  Eigen::MatrixXd Z_values = pca.getEigenVectors().cast<double>().col(2).transpose() * processed_cloud.topRows(3);
+  processed_cloud.row(0) = X_values;
+  processed_cloud.row(1) = Y_values;
+  processed_cloud.row(2) = Z_values;
+
+
+  // Some test
+  pcl::PointCloud<pcl::PointXYZI> new_cloud;
+  for(int i = 0, i_end = processed_cloud.cols(); i != i_end; i++)
+  {
+    pcl::PointXYZI new_point;
+    new_point.x = processed_cloud(0, i);
+    new_point.y = processed_cloud(1, i);
+    new_point.z = processed_cloud(2, i);
+    new_point.intensity = processed_cloud(3, i);
+    new_cloud.push_back(new_point);
+  }
+  
+  pca.setInputCloud(new_cloud.makeShared());
+  std::cout << pca.getEigenVectors() << std::endl;
   Eigen::Vector4d cloud_centroid; // (x, y, z, 1)
-  if(!pcl::compute3DCentroid(input_cloud, cloud_centroid))
+  if(!pcl::compute3DCentroid(new_cloud, cloud_centroid))
   {
     std::cout << "Failed to compute cloud centroid." << std::endl;
     return(-1);
   }
-  pcl::demeanPointCloud(input_cloud, cloud_centroid, processed_cloud);
-
-  // Do PCA and re-alignment
-  pcl::PCA<pcl::PointXYZI> pca(false);
-  pca.setInputCloud(processed_cloud.makeShared());
-  
-  
-  std::cout << pca.getEigenVectors() << std::endl;
-
-
+  std::cout << cloud_centroid << std::endl;
+  // pcl::io::savePCDFileBinary("processed_" + f_pcd, processed_cloud);
   return(0);
 }
